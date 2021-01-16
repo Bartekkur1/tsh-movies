1abf14b0cd4eae10160496cca8592dbdaee52f4a
bartekkur1 <bartekkur1@gmail.com>
2021-01-17 00:36:48 +0100

    Movie creation integration test name correction

--- tests/integration/movieCreation.spec.ts
+++ tests/integration/movieCreation.spec.ts
@@ -49,76 +49,76 @@ describe("Movie creation rest api", () => {
     });
 
     // - a list of genres (only predefined ones from db file) (required, array of predefined strings)
-    it("try to save movie with unknown genres", () => {
+    it("try to save movie with unknown genres results in error", () => {
         addMovieAndExpectError({ ...mockMovie, genres: ["Invalid", "Genres"] }, "Movie contains unknown genres!");
     });
 
-    it("try to save movie with empty genres", () => {
+    it("try to save movie with empty genres results in error", () => {
         addMovieAndExpectError({ ...mockMovie, genres: [] }, "Movie genres cannot be empty!");
     });
 
-    it("try to save movie without genres", () => {
+    it("try to save movie without genres results in error", () => {
         const movieWihtoutGenres = mockMovie as any;
         delete movieWihtoutGenres["genres"];
         addMovieAndExpectError(movieWihtoutGenres, "Movie must contain genres!");
     });
 
     // - title (required, string, max 255 characters)
-    it("try to save movie without title", () => {
+    it("try to save movie without title results in error", () => {
         const movieWithoutTitle = mockMovie as any;
         delete movieWithoutTitle["title"];
         addMovieAndExpectError(movieWithoutTitle, "Movie title is required!");
     });
 
-    it("try to save movie with empty title", () => {
+    it("try to save movie with empty title results in error", () => {
         addMovieAndExpectError({ ...mockMovie, title: "" }, "Movie title cannot be empty!");
     });
 
-    it("try to save movie with too long title", () => {
+    it("try to save movie with too long title results in error", () => {
         const toolongTitle = "tooloooong".repeat(30);
         addMovieAndExpectError({ ...mockMovie, title: toolongTitle }, "Movie title is too long!");
     });
 
     // - year (required, number)
 
-    it("try to save movie without year", () => {
+    it("try to save movie without year results in error", () => {
         const movieWihtoutYear = mockMovie as any;
         delete movieWihtoutYear["year"];
         addMovieAndExpectError(movieWihtoutYear, "Movie year is required!");
     });
 
-    it("try to save movie with year as string", () => {
+    it("try to save movie with year as string results in error", () => {
         addMovieAndExpectError({ ...mockMovie, year: "trust_me_Im_a_year" }, "Movie title must be a number!");
     });
 
     // - runtime (required, number)
 
-    it("try to save movie without runtime", () => {
+    it("try to save movie without runtime results in error", () => {
         const movieWithoutRuntime = mockMovie as any;
         delete movieWithoutRuntime["runtime"];
         addMovieAndExpectError(movieWithoutRuntime, "Movie runtime is required!");
     });
 
-    it("try to save movie with runtime as string", () => {
+    it("try to save movie with runtime as string results in error", () => {
         addMovieAndExpectError({ ...mockMovie, runtime: "trust_me_Im_a_runtime" }, "Movie runtime must be a number!");
     });
 
     // - director (required, string, max 255 characters)
 
-    it("try to save movie without director", () => {
+    it("try to save movie without director results in error", () => {
         const movieWithoutDirector = mockMovie as any;
         delete movieWithoutDirector["director"];
         addMovieAndExpectError(movieWithoutDirector, "Movie director is required!");
     });
 
-    it("try to save movie with too long director", () => {
+    it("try to save movie with too long director results in error", () => {
         const directorName = "Too Long John".repeat(20);
         addMovieAndExpectError({ ...mockMovie, director: directorName }, "Movie director is too long!");
     });
 
     // - actors (optional, string)
 
-    it("Save movie without actors", () => {
+    it("Save movie without actors and expects to save it", () => {
         const movieWithoutActors = mockMovie as any;
         delete movieWithoutActors["actors"];
         addMovieAndExpectMoviesCountIncreased(movieWithoutActors);
@@ -126,7 +126,7 @@ describe("Movie creation rest api", () => {
 
     // - plot (optional, string)
 
-    it("Save movie without plot", () => {
+    it("Save movie without plot and expects to save it", () => {
         const movieWithoutPlot = mockMovie as any;
         delete movieWithoutPlot["plot"];
         addMovieAndExpectMoviesCountIncreased(movieWithoutPlot);
@@ -134,7 +134,7 @@ describe("Movie creation rest api", () => {
 
     // - posterUrl (optional, string)
 
-    it("Save movie without posterUrl", () => {
+    it("Save movie without posterUrl and expects to save it", () => {
         const movieWithoutPosterUrl = mockMovie as any;
         delete movieWithoutPosterUrl["posterUrl"];
         addMovieAndExpectMoviesCountIncreased(movieWithoutPosterUrl);
